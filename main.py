5007b743c1e8d5518a8083ba550bf63305cc972b
zewa-bamuga <tikhonov.igor2028@yandex.ru>
2024-07-12 11:22:08 +0700

    Ready-made project with .exe

--- main.py
+++ main.py
@@ -2,65 +2,16 @@ import csv
 import asyncio
 import os
 import time
-from dotenv import load_dotenv
 
 from telethon import TelegramClient
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.types import PeerUser, PeerChannel
 
-load_dotenv()
 
-api_id = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-phone = os.getenv('PHONE')
-
-async def main():
-    start_time = time.time()
-    client = TelegramClient(phone, api_id, api_hash)
-    await client.start()
-
-    print("Введите ссылку на чат или канал для парсинга сообщений и членов группы:")
-    chat_url = input("Введите ссылку: ")
-
-    # Извлечение идентификатора чата или канала из ссылки
-    if 'https://web.telegram.org/a/#' in chat_url:
-        chat_id = chat_url.split('#')[-1]
-        if chat_id.startswith('-'):
-            chat_id = int(chat_id)
-        else:
-            chat_id = int(f'-100{chat_id}')
-    elif 'https://t.me/' in chat_url:
-        username = chat_url.split('/')[-1]
-        try:
-            entity = await client.get_entity(username)
-            if entity.broadcast:  # Это канал
-                target_group = entity
-            elif entity.megagroup:  # Это мега-группа
-                target_group = entity
-            else:
-                print("Неподдерживаемый тип чата или канала")
-                return
-        except Exception as e:
-            print(f"Не удалось получить информацию о чате или канале: {e}")
-            return
-    else:
-        print("Неправильный формат ссылки")
-        return
-
-    try:
-        if not 'target_group' in locals():
-            target_group = await client.get_entity(PeerChannel(chat_id))
-    except Exception as e:
-        print(f"Не удалось получить информацию о чате или канале: {e}")
-        return
-
-    print("Узнаём пользователей, которые писали сообщения...")
-
+async def fetch_messages_and_participants(client, target_group, limit=1100):
     offset_id = 0
-    limit = 10
     all_messages = []
     total_messages = 0
-    total_count_limit = 10
     user_ids = set()
 
     while True:
@@ -77,41 +28,97 @@ async def main():
         if not history.messages:
             break
         messages = history.messages
-        for message in messages:
-            if isinstance(message.from_id, PeerUser):
-                user_ids.add(message.from_id.user_id)
-            all_messages.append(message.message)
+        user_ids.update({message.from_id.user_id for message in messages if isinstance(message.from_id, PeerUser)})
+        all_messages.extend([message.message for message in messages])
         offset_id = messages[-1].id
         total_messages += len(messages)
-        if total_count_limit != 0 and total_messages >= total_count_limit:
+        if total_messages >= limit:
             break
-
-    print("Сохраняем данные о пользователях в файл...")
 
     all_participants = await client.get_participants(target_group)
     participants_dict = {user.id: user for user in all_participants if user.id in user_ids}
 
-    with open("active_members.csv", "w", encoding="UTF-8") as f:
-        writer = csv.writer(f, delimiter=",", lineterminator="\n")
-        writer.writerow(["username", "name", "group"])
-        for user_id in user_ids:
-            user = participants_dict.get(user_id)
-            if user:
-                username = user.username if user.username else ""
-                first_name = user.first_name if user.first_name else ""
-                last_name = user.last_name if user.last_name else ""
-                name = (first_name + ' ' + last_name).strip()
-                writer.writerow([username, name, target_group.title])
+    return user_ids, participants_dict
 
-    print("Парсинг активных участников группы успешно выполнен.")
 
-    print("Сохраняем данные о сообщениях в файл...")
-    with open("chats.csv", "w", encoding="UTF-8") as f:
-        writer = csv.writer(f, delimiter=",", lineterminator="\n")
-        for message in all_messages:
-            writer.writerow([message])
+async def parse_telegram_chat(api_id, api_hash, phone, chat_url, limit=1100):
+    async with TelegramClient(phone, api_id, api_hash) as client:
+        if 'https://web.telegram.org/a/#' in chat_url:
+            chat_id = chat_url.split('#')[-1]
+            if chat_id.startswith('-'):
+                chat_id = int(chat_id)
+            else:
+                chat_id = int(f'-100{chat_id}')
+        elif 'https://t.me/' in chat_url:
+            username = chat_url.split('/')[-1]
+            try:
+                entity = await client.get_entity(username)
+                if entity.broadcast:
+                    target_group = entity
+                elif entity.megagroup:
+                    target_group = entity
+                else:
+                    print("Неподдерживаемый тип чата или канала")
+                    return
+            except Exception as e:
+                print(f"Не удалось получить информацию о чате или канале: {e}")
+                return
+        else:
+            print("Неправильный формат ссылки")
+            return
 
-    print('Парсинг сообщений группы успешно выполнен.')
-    print(f"Время выполнения парсинга: {time.time() - start_time:.2f} секунд")
+        try:
+            if not 'target_group' in locals():
+                target_group = await client.get_entity(PeerChannel(chat_id))
+        except Exception as e:
+            print(f"Не удалось получить информацию о чате или канале: {e}")
+            return
 
-asyncio.run(main())
+        user_ids, participants_dict = await fetch_messages_and_participants(client, target_group, limit=limit)
+
+        target_title = target_group.title if hasattr(target_group, 'title') else 'Unknown Group'
+
+        return user_ids, participants_dict, target_title
+
+
+async def main():
+    api_id =
+    api_hash = ""
+    phone = ""
+
+    start_time = time.time()
+
+    current_directory = os.getcwd()
+    print("Текущая директория проекта:", current_directory)
+
+    print("Введите ссылку на чат или канал для парсинга сообщений и членов группы:")
+    chat_url = input("Введите ссылку: ")
+
+    try:
+        user_ids, participants_dict, target_title = await parse_telegram_chat(api_id, api_hash, phone, chat_url)
+
+        if participants_dict:
+            with open("active_members.csv", "w", encoding="UTF-8") as f:
+                writer = csv.writer(f, delimiter=",", lineterminator="\n")
+                writer.writerow(["username", "name", "group"])
+                for user_id in user_ids:
+                    user = participants_dict.get(user_id)
+                    if user:
+                        username = user.username if user.username else ""
+                        first_name = user.first_name if user.first_name else ""
+                        last_name = user.last_name if user.last_name else ""
+                        name = (first_name + ' ' + last_name).strip()
+                        writer.writerow([username, name, target_title])
+
+            print("Парсинг активных участников группы успешно выполнен.")
+        else:
+            print("Нет доступных участников для записи в CSV.")
+
+        print(f"Время выполнения парсинга: {time.time() - start_time:.2f} секунд")
+
+    except ValueError as ve:
+        print(f"Ошибка: {ve}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
